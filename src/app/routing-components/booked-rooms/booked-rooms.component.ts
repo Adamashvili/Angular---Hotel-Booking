bc0fb0d15e1887db7222779fd23d3f96595fdeb8
Irakli Adamashvili <adamashvili.irakli8@gmail.com>
2025-07-01 20:37:11 +0400

    update bookingArea

--- src/app/routing-components/booked-rooms/booked-rooms.component.ts
+++ src/app/routing-components/booked-rooms/booked-rooms.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BookedService } from 'src/app/services/bookedApi.service';
 import { HotelsService } from 'src/app/services/hotelsApi.service';
+import { RoomsService } from 'src/app/services/roomsApi.service';
 
 @Component({
     selector: 'app-booked-rooms',
@@ -10,16 +11,15 @@ import { HotelsService } from 'src/app/services/hotelsApi.service';
     standalone: false
 })
 export class BookedRoomsComponent implements OnInit {
-  constructor (private bookedHttpServ: BookedService, private hotel: HotelsService ) {}
+  constructor (private bookedHttpServ: BookedService, private roomsServ: RoomsService ) {}
 
-  table: any = []
+  bookedList: any = []
 
 
 
   ngOnInit(): void {
     this.getBookedRooms()
-    console.log(this.table.id);
-    
+
     
   }
 
@@ -27,15 +27,43 @@ export class BookedRoomsComponent implements OnInit {
 
 
   getBookedRooms() {
+    this.bookedList = []
     return this.bookedHttpServ.getRooms().subscribe({
-      next: ((res) => this.table = res)
+      next: ((res: any[]) => {
+        res.forEach( (bookedItem:any) => {
+       
+          
+          this.roomsServ.getRooms().subscribe((roomData: any[]) => {
+            roomData.forEach( (roomItem: any) => {
+             
+              
+              if(bookedItem.roomID == roomItem.id) {
+                bookedItem.room = roomItem.name
+                bookedItem.hotelId = roomItem.hotelId
+                bookedItem.pricePerNight = roomItem.pricePerNight
+                bookedItem.image = roomItem.images[0].source
+                bookedItem.customerName = bookedItem.customerName.slice(0, 20)
+                this.bookedList.push(bookedItem)
+                // 
+                
+              }
+              
+            } )
+            
+          })
+        } )
+        
+      })
     })
   }
 
   cancelBooking(id:any) {
     return this.bookedHttpServ.deleteRoom(id).subscribe(
       {
-        next: (res) => alert(res),
+        next: (res) => {
+          alert(res)
+          this.getBookedRooms()
+        },
         error:(err) => alert(err.error),
         
         
